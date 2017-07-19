ba78c6273914f254026e24b6bca6ee3f3ab8e650
Manu Arora <mannuarora7000@gmail.com>
2017-07-19 11:10:48 +0000

    Did the same thing with pointers

--- swap.c
+++ swap.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 //Program to swap two integers
+void swap(int *c,int *d);
 int main()
 {
   int a, b;
   printf("Enter 2 numbers");
   scanf("%d %d", &a, &b);
-
-  int temp = a;
-  a = b;
-  b = temp;
+  
+  swap(&a, &b);
 
   printf("swapping...\n");
 
   printf("a = %d, b = %d\n", a, b);
+}
 
-
+void swap(int *c, int *d)
+{
+  int temp = *c;
+  *c =*d;
+  *d = temp;
 }
