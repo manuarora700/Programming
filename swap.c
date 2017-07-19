52c59c097f88170f1af63123b459d8c4365cc4df
Manu Arora <mannuarora7000@gmail.com>
2017-07-19 11:05:06 +0000

    Added comments

--- swap.c
+++ swap.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-
+//Program to swap two integers
 int main()
 {
   int a, b;
   printf("Enter 2 numbers");
-  scanf("%d %d", &n, &b);
+  scanf("%d %d", &a, &b);
 
   int temp = a;
   a = b;
