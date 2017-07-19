4c5fec06fb1a886a1ed3a5f0ead99dc032abc99e
Manu Arora <mannuarora700@gmail.com>
2017-07-19 16:21:02 +0530

    Program for swapping 2 numbers
    ...

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+  int a, b;
+  printf("Enter 2 numbers");
+  scanf("%d %d", &n, &b);
+
+  int temp = a;
+  a = b;
+  b = temp;
+
+  printf("swapping...\n");
+
+  printf("a = %d, b = %d\n", a, b);
+
+
+}
