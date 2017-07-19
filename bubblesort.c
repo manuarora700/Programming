3085911367932af4d40e0424a2976e8eeab9694f
Manu Arora <mannuarora7000@gmail.com>
2017-07-19 11:51:11 +0000

    Added comments for every loop

--- bubblesort.c
+++ bubblesort.c
@@ -2,20 +2,22 @@
 
 int main(void)
 {
-    //int swap_counter = -1;
     int n;
     
+    // Array declared to store integers
     int array[1000];
     printf("Enter number of integers to be sorted\n");
     scanf("%d", &n);
     
     printf("Enter numbers: \n");
     
+    // for loop to push values into the array.
     for(int i = 0; i < n; i++)
     {
         scanf("%d", &array[i]);
     }
     
+    // For loop to print entered values in graphical array representation
     for(int j = 0; j < n; j++)
     {
     printf("%d | ", array[j]);
@@ -23,12 +25,17 @@ int main(void)
     
     printf("\n");
     
+    // for loop to iterate over the complete array
     for(int j = 0; j < n; j++)
     {
+        
+        // for loop to check two adjacent elements in the array
         for(int k = 0; k < n-j-1; k++)
         {
+            //if left is greater than right then swap.
             if(array[k] > array[k+1])
             {
+                // 3 lines of code to swap.
                 int temp = array[k];
                 array[k] = array[k+1];
                 array[k+1] = temp;
@@ -37,6 +44,8 @@ int main(void)
     }
     printf("Sorted list is: \n");
     
+    
+    // for loop to print array in graphical form.
     for(int l = 0; l < n; l++)
     {
         printf("%d |", array[l]);
