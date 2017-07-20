de8dde3f2ce3ae932f0f22dd12555b6584959f89
Manu Arora <mannuarora700@gmail.com>
2017-07-20 17:49:47 +0000

    Insertion Sort

---
+++ insertionsort.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, d;
+    int array[100];
+    printf("Enter the number of elements: \n");
+    
+    scanf("%d", &n);
+    
+    printf("Enter the values to be sorted: \n");
+    
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    
+    printf("You entered: \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("| %d ", array[i]);
+    }
+    printf("|\n");
+    
+    for(int i = 1; i < n; i++)
+    {
+        d = i;
+        while(d > 0 && array[d] < array[d-1])
+        {
+            int temp = array[d];
+            array[d] = array[d-1];
+            array[d-1] = temp;
+            
+            d--;
+        }
+    }
+    
+    printf("Sorted array: \n");
+    for(int i = 0; i < n; i++)
+    {
+        printf("| %d ", array[i]);
+    }
+    printf("|\n");
+}
