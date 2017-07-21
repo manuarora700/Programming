fe2bb057fc0e1842fd0a82aab682788586ac3374
Manu Arora <mannuarora700@gmail.com>
2017-07-21 07:11:52 +0000

    Added comments

--- insertionsort.c
+++ insertionsort.c
@@ -2,11 +2,13 @@
 
 int main()
 {
+    // Declaring variables and an array
     int n, d;
     int array[100];
     printf("Enter the number of elements: \n");
     
     scanf("%d", &n);
+    // Getting values for the array
     
     printf("Enter the values to be sorted: \n");
     
@@ -14,6 +16,7 @@ int main()
     {
         scanf("%d", &array[i]);
     }
+    //Printing array in array graphical form
     
     printf("You entered: \n");
     for(int i = 0; i < n; i++)
@@ -22,9 +25,15 @@ int main()
     }
     printf("|\n");
     
+    //Actual code for insertion sort
+    // For loop to iterate through the array once
     for(int i = 1; i < n; i++)
     {
+        // Setting first elemented to sorted || d = i;
+        
         d = i;
+        
+        // Swaps the values only if array[d] < array[d-1] and then decrements the value of d
         while(d > 0 && array[d] < array[d-1])
         {
             int temp = array[d];
