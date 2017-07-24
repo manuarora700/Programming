0aa87756489299dc440db2e9621ae569d548b55c
Manu Arora <mannuarora700@gmail.com>
2017-07-24 18:13:30 +0000

    Stack operations with the help of array and an integer value. 1. push() 2. pop() 3. display()

--- stack_array.c
+++ stack_array.c
@@ -1,17 +1,30 @@
+/* Stack operations using array and integer value
+3 functions in total:
+    a. push();
+    b. pop();
+    c. display();
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
+// maximum size of the stack
 #define MAXSIZE 5
 
+
+// structure of the stack
 typedef struct _stack
 {
+    // array containing stack value
     int stk[MAXSIZE];
+    // takes care of the top of the stack
     int top;
 }
 stack;
 
 stack s;
 
+// function declarations
 void push(void);
 int pop(void);
 void display(void);
@@ -20,8 +33,10 @@ int main()
 {
     int choice;
     int option = 1;
+    // top set to -1 for further operations
     s.top = -1;
     printf("STACK OPERATIONS\n");
+    // loop to ask the user for operations over and over again.
     while(option)
     {
         printf("\n-------------------------\n");
@@ -62,6 +77,8 @@ int main()
 void push(void)
 {
     int num;
+    
+    // if the top element is 1 less than the max size then the stack is full.
     if(s.top == (MAXSIZE - 1))
     {
         printf("Stack is full\n");
@@ -72,7 +89,9 @@ void push(void)
         printf("Enter the element to be pushed: ");
         scanf("%d", &num);
         
+        // adds 1 index to the top position so that the next element comes on top
         s.top = s.top + 1;
+        // saves the new value(input) on the array at index s.top
         s.stk[s.top] = num;
         
         printf("Number successfully pushed to the stack\n");
@@ -83,6 +102,8 @@ void push(void)
 int pop(void)
 {
     int num;
+    
+    // if the top value is -1 then stack is already empty.
     if(s.top == -1)
     {
         printf("Stack is empty already bruh!\n");
@@ -90,8 +111,11 @@ int pop(void)
     }
     else
     {
+        // sets num value to s.stk top value so that the next push value can simply overwrite this current value.
         num = s.stk[s.top];
         printf("Popped element is = %d", s.stk[s.top]);
+        
+        // decrements the top indicating the one less position can be overwritten.
         s.top = s.top - 1;
     }
     return (num);
@@ -110,7 +134,7 @@ void display(void)
     else
     {
         printf("\nStatus of the stack is \n");
-        
+        // iterates over the s.stk array to print out values from first to last in the form of a stack.
         for(i = s.top; i >=0; i--)
         {
             printf("%d\n", s.stk[i]);
