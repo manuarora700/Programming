5759a8584f2ef56d74b20662f81bd1f1c5a188f5
Manu Arora <mannuarora700@gmail.com>
2018-06-22 22:01:11 +0530

    Going towards express.js

--- node/server.js
+++ node/server.js
@@ -1,8 +1,16 @@
 const http = require('http');
 
 
-const server = http.createServer(() => {
-	console.log('I hear you, Thankyou for the request');
+const server = http.createServer((request, response) => {
+	console.log('headers', request.headers);
+	console.log('method', request.method);
+	console.log('url', request.url);
+	const user = {
+		name: 'John',
+		hobby: 'Skating'
+	}
+	response.setHeader('Content-Type', 'application/json');
+	response.end(JSON.stringify(user));
 
 })
 
