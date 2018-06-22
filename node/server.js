320e22b9a7506100bf676a719b124352513e0b02
Manu Arora <mannuarora700@gmail.com>
2018-06-23 00:13:56 +0530

    Working with express

--- node/server.js
+++ node/server.js
@@ -1,17 +1,13 @@
-const http = require('http');
+const express = require('express');
 
 
-const server = http.createServer((request, response) => {
-	console.log('headers', request.headers);
-	console.log('method', request.method);
-	console.log('url', request.url);
+const app = express();
+
+app.get('/', (req,res) => {
 	const user = {
-		name: 'John',
-		hobby: 'Skating'
+		name: 'Sally',
+		hobby: 'in my alley'
 	}
-	response.setHeader('Content-Type', 'application/json');
-	response.end(JSON.stringify(user));
-
+	res.send(user);
 })
-
-server.listen(3000);
+app.listen(3000);
