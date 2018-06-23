883ee927fd6e53eef39bb16e36fa03b3de483249
Manu Arora <mannuarora700@gmail.com>
2018-06-24 00:55:15 +0530

    Playing with node postman express and other stuff

--- node/server.js
+++ node/server.js
@@ -1,25 +1,18 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
-	
+
+app.get('/:id', (req,res) => {
+	// console.log(req.query);
+	//req.body
+	// console.log(req.header);	
+	console.log(req.params)
 	res.send('getting root');
 });
 
-app.get('/profile', (req,res) => {
-	
-	res.send('getting profile');
-});
-
-app.post('/profile', (req,res) => {
-	console.log(req.body);
-	const user = {
-		name: 'Sally',
-		hobby: 'Soccer'
-	}
-	res.send(user);
-});
 app.listen(3000);
