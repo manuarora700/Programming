8a30a44cab933d78b39c744f3f1cacfe7adcda69
Manu Arora <mannuarora700@gmail.com>
2018-06-24 01:00:14 +0530

    serving static html files

--- node/server.js
+++ node/server.js
@@ -3,16 +3,5 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json());
-
-
-app.get('/:id', (req,res) => {
-	// console.log(req.query);
-	//req.body
-	// console.log(req.header);	
-	console.log(req.params)
-	res.send('getting root');
-});
-
+app.use(express.static(__dirname + '/public'))
 app.listen(3000);
